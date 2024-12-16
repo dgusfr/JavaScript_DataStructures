89d27dd07392b267edcd73b043b24aedbb59983f
dgusfr <diegogustavo.fr@gmail.com>
2024-12-16 18:53:03 -0300

    feat(update): leghtn

---
+++ splitArray.js
@@ -0,0 +1,8 @@
+const students = [
+  "Alice", "Bob", "Charlie", "Diana", "Ethan", 
+  "Fiona", "George", "Hannah", "Ian", "Julia", 
+  "Kevin", "Laura", "Michael", "Nina", "Oliver", 
+  "Paula", "Quentin", "Rachel", "Steve", "Tina"
+];
+
+console.log(students);
