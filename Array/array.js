2fbc3ee09c08a505d6073a8d2f7a5ae79d4bc832
dgusfr <diegogustavo.fr@gmail.com>
2024-12-23 09:48:53 -0300

    feta(update): for each

--- Array/array.js
+++ Array/array.js
@@ -1,33 +1,4 @@
 const frutas = ['maçã', 'banana', 'laranja'];
 
-frutas.push('uva');
-frutas.pop();
-frutas.unshift('abacaxi');
-frutas.shift();
-console.log(frutas[1]);
-frutas[1] = 'manga';
-console.log(frutas.length);
-
-for (let i = 0; i < frutas.length; i++) {
-  console.log(frutas[i]);
-}
-
 frutas.forEach(fruta => console.log(fruta));
 
-const filtradas = frutas.filter(fruta => fruta.includes('a'));
-const emMaiusculas = frutas.map(fruta => fruta.toUpperCase());
-const concatFrutas = frutas.reduce((acc, fruta) => acc + ', ' + fruta, '');
-
-console.log(frutas.includes('banana'));
-
-const encontrada = frutas.find(fruta => fruta.startsWith('m'));
-const ordenadas = frutas.sort();
-const reverso = frutas.reverse();
-const vegetais = ['cenoura', 'batata'];
-const alimentos = frutas.concat(vegetais);
-
-frutas.splice(1, 1);
-
-const fatia = frutas.slice(1, 3);
-const comoString = frutas.join(', ');
-const preenchido = new Array(5).fill('abacate');
