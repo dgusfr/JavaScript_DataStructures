d3c1589f0548c3937d437eb3f1c3cb200d492f66
dgusfr <diegogustavo.fr@gmail.com>
2024-12-23 10:10:17 -0300

    feat add filter methods

--- Array/array.js
+++ Array/array.js
@@ -5,3 +5,13 @@ const notas = [10, 9.5, 8, 7, 6]
 const notasAtualizadas = notas.map(nota => nota + 1 >= 10 ? 10 : nota + 1);
 console.log(notasAtualizadas)
 
+const nomes = ['Ana', 'Bia', 'Gui', 'Lia', 'Rafa'];
+const nomeCaixaAlta = nomes.map(nome => nome.toUpperCase());
+console.log(nomeCaixaAlta);
+
+const alunos = ['Diego', 'Maria', 'José', 'João', 'Pedro'];
+const medias = [7, 4.5, 8, 9, 6]
+const reprovados = alunos.filter((aluno, indice) =>{
+  return medias[indice] < 7 
+})
+console.log(reprovados)
