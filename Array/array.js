e37baeed8e544d231fcbdc6d52ed2439c3786949
dgusfr <diegogustavo.fr@gmail.com>
2024-12-23 09:53:40 -0300

    feat add array notes

--- Array/array.js
+++ Array/array.js
@@ -1,4 +1,7 @@
 const frutas = ['maçã', 'banana', 'laranja'];
-
 frutas.forEach(fruta => console.log(fruta));
+
+const notas = [10, 9.5, 8, 7, 6]
+const notasAtualizadas = notas.map(nota => nota + 1);
+console.log(notasAtualizadas)
 
