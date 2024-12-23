338082673df2a66ba77cf27d089b21d4a93df8ef
dgusfr <diegogustavo.fr@gmail.com>
2024-12-23 09:55:56 -0300

    feat add map

--- Array/array.js
+++ Array/array.js
@@ -2,6 +2,6 @@ const frutas = ['maçã', 'banana', 'laranja'];
 frutas.forEach(fruta => console.log(fruta));
 
 const notas = [10, 9.5, 8, 7, 6]
-const notasAtualizadas = notas.map(nota => nota + 1);
+const notasAtualizadas = notas.map(nota => nota + 1 >= 10 ? 10 : nota + 1);
 console.log(notasAtualizadas)
 
