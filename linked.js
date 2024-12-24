424991cae8246a14e62b7be6493d36cdc9356cac
dgusfr <diegogustavo.fr@gmail.com>
2024-12-24 08:46:06 -0300

    feat: adicionar método para remover ciclos na Linked List

--- linked.js
+++ linked.js
@@ -8,3 +8,23 @@ LinkedList.prototype.hasCycle = function () {
   }
   return false;
 };
+LinkedList.prototype.removeCycle = function () {
+  if (!this.hasCycle()) return;
+
+  let slow = this.head;
+  let fast = this.head;
+
+  while (fast && fast.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow === fast) break;
+  }
+
+  slow = this.head;
+  while (slow.next !== fast.next) {
+      slow = slow.next;
+      fast = fast.next;
+  }
+
+  fast.next = null;
+};
