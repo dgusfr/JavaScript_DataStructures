8365af3d82663e28e73705e232d2f644e36b566b
dgusfr <diegogustavo.fr@gmail.com>
2024-12-30 09:46:11 -0300

    feat: implementar menu de opções para o usuário

--- set.js
+++ set.js
@@ -4,3 +4,13 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+function displayMenu() {
+  console.log(`\nEscolha uma operação para realizar no Set:`);
+  console.log("1. Adicionar um elemento");
+  console.log("2. Remover um elemento");
+  console.log("3. Verificar se um elemento existe");
+  console.log("4. Listar todos os elementos");
+  console.log("5. Limpar o Set");
+  console.log("6. Sair\n");
+}
